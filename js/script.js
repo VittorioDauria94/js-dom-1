535bd1fff6c3c59a63d122304b7b341cebc6a79b
VittorioDauria94 <dauriavittorio94@gmail.com>
2025-09-28 15:18:16 +0200

    fix: added .toogle for cleaner code

--- js/script.js
+++ js/script.js
@@ -4,15 +4,12 @@ const litLamp = document.querySelector(".yellow-lamp");
 const onOffButton = document.getElementById("on-off-button");
 
 onOffButton.addEventListener("click", function () {
+  bodyElem.classList.toggle("background-black");
+  unlitLamp.classList.toggle("display-none");
+  litLamp.classList.toggle("display-block");
   if (onOffButton.innerHTML === "Off") {
-    bodyElem.classList.remove("background-black");
-    unlitLamp.classList.add("display-none");
-    litLamp.classList.add("display-block");
     onOffButton.innerHTML = "On";
   } else {
-    bodyElem.classList.add("background-black");
-    unlitLamp.classList.remove("display-none");
-    litLamp.classList.remove("display-block");
     onOffButton.innerHTML = "Off";
   }
 });
